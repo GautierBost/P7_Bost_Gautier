3bafd5c951f9b047ee17ac3fac1eaa696550b72f
Gautier BOST <pti_gautier@hotmail.fr>
2022-07-13 20:13:17 +0200

    index+login+signup

---
+++ pages/SignUp.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page">
+    <div class="block">
+      <Auth :type="authType" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUpPage",
+  data() {
+    return {
+      authType: "Inscription",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.page {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #eeeeee;
+}
+
+.block {
+  width: 400px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page">
+    <img class="logo" src="/logo.svg" alt="logo" />
+    <h1>Groupomania</h1>
+    <div class="links">
+      <NuxtLink to="Login">Connexion</NuxtLink>
+      <NuxtLink to="SignUp">Inscription</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "indexPage",
+};
+</script>
+
+<style scoped>
+.page {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #eeeeee;
+}
+
+.logo {
+  width: 200px;
+  transform: rotate(0deg);
+  transition: 2s cubic-bezier(0.58, -0.48, 0.32, 1.41) transform;
+}
+
+.logo:hover {
+  transform: rotate(360deg);
+}
+
+h1 {
+  color: #fd2d01;
+  font-size: 100px;
+}
+
+.links {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+a {
+  text-decoration: none;
+  color: #4e5166;
+  border-radius: 100vh;
+  padding: 10px;
+  font-size: 25px;
+  background: linear-gradient(to bottom right, #fd2d01, #ffd7d7);
+  box-shadow: 0 6px 12px #505050;
+  transform: scale(1);
+  transition: 500ms transform;
+}
+
+a:hover {
+  transform: scale(1.2);
+}
+</style>
